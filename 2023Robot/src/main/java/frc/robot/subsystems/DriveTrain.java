75e22f2757fa0f332780abe80c27a3b511351c79
v0ncent <83373495+v0ncent@users.noreply.github.com>
2023-02-07 17:33:06 -0600

    Created drivetrain subsystem file

---
+++ 2023Robot/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveTrain extends SubsystemBase {
+
+    @Override
+    public void periodic(){
+
+    }
+    
+}
