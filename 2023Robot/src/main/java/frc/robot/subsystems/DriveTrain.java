310dfbfa0e1afb9806472d125027c99b0c4f7d98
emeof <emeof489@gmail.com>
2023-02-18 13:09:59 -0600

    started working on drivetrain lol

--- 2023Robot/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ 2023Robot/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,8 +1,15 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
+
+    private final CANSparkMax frontRightMotor;
+    private final CANSparkMax backRightMotor;
+    private final CANSparkMax frontLeftMotor;
+    private final CANSparkMax backLeftMotor;
 
     @Override
     public void periodic(){
