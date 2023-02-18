b571662120131d02bfc84c1d12c75f72049885c2
yourmoms-boyfriend <25kvang5883@apps-hcpak12.org>
2023-02-18 12:56:27 -0600

    Made camarara variables

--- 2023Robot/src/main/java/frc/robot/subsystems/Camera.java
+++ 2023Robot/src/main/java/frc/robot/subsystems/Camera.java
@@ -1,9 +1,36 @@
 package frc.robot.subsystems;
 
+import org.opencv.core.Mat;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Camera extends SubsystemBase{
     
-    public Camera(){}
+    private final UsbCamera camera;
+    private final CvSink cvSink;
+    private final CvSource outputstream;
+
+    private final Mat source;
+    private final Mat output;
+
+    private final int width = 640;
+    private final int height = 480;
+
+    public Camera(){
+
+        camera = CameraServer.startAutomaticCapture();
+        camera.setResolution(width,height);
+        cvSink = CameraServer.getVideo();
+        outputstream = CameraServer.putVideo("RobotPOV", width,height);
+
+        source = new Mat();
+        output = new Mat();
+    }
+
+
 
     @Override
     public void periodic() {
