c1c3df09b00881afaa406d0d17df047389affa6b
emeof <emeof489@gmail.com>
2023-02-18 12:18:36 -0600

    i made a camera breach a subsystem

---
+++ 2023Robot/src/main/java/frc/robot/subsystems/Camera.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+public class Camera extends SubsystemBase{
+    
+    public Camera(){}
+
+    @Override
+    public void periodic() {
+    // This method will be called once per scheduler run
+
+
+
+    }
+
+
+    @Override
+    public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+
+
+
+    }
+
+
+
+
+}
