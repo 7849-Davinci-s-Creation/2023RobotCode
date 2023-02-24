0d46a27b4452e8f9b947e5d417896b3e73177622
emeof <emeof489@gmail.com>
2023-02-24 16:12:21 -0600

    im pushing again  e e e e e  e

--- 2023Robot/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ 2023Robot/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -7,13 +7,15 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
-
+    // create motor objects
     CANSparkMax frontRightMotor;
     CANSparkMax backRightMotor;
     CANSparkMax frontLeftMotor;
     CANSparkMax backLeftMotor;
+    // create motor control group
     MotorControllerGroup RightDrive;
     MotorControllerGroup LeftDrive;
+    // create differential drive
     DifferentialDrive differentialDrive;
 
     @Override
