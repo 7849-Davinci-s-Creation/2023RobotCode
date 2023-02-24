536792e96e39e2836c1e4f5e025b1d6af1acb9e6
KantancheeKIRA <Kantancheekhang@gmail.com>
2023-02-24 16:26:32 -0600

    shut up johnathon

---
+++ 2023Robot/src/main/java/frc/robot/commands/SwitchCamera.java
@@ -0,0 +1,23 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class SwitchCamera extends CommandBase {
+    
+    @Override
+    public void initialize( ){
+    }
+
+    @Override
+    public void execute( ){
+    }
+
+    @Override
+    public void end(boolean interuppted) {
+    }
+
+    @Override 
+    public boolean isFinished( ){
+        return false;
+    }
+}
