195efeb083098fdd3964a506d20584ba5ec8882e
emeof <emeof489@gmail.com>
2023-02-24 16:04:43 -0600

    can i commit

--- 2023Robot/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ 2023Robot/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
@@ -10,6 +12,9 @@ public class DriveTrain extends SubsystemBase {
     CANSparkMax backRightMotor;
     CANSparkMax frontLeftMotor;
     CANSparkMax backLeftMotor;
+    MotorControllerGroup RightDrive;
+    MotorControllerGroup LeftDrive;
+    DifferentialDrive differentialDrive;
 
     @Override
     public void periodic(){
