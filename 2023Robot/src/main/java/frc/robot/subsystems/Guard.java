de4f08422cb9dd8c2d017dc384974155af01deab
v0ncent <83373495+v0ncent@users.noreply.github.com>
2023-03-25 20:47:42 -0500

    Changed can ids

--- 2023Robot/src/main/java/frc/robot/subsystems/Guard.java
+++ 2023Robot/src/main/java/frc/robot/subsystems/Guard.java
@@ -11,15 +11,17 @@ public class Guard extends SubsystemBase {
 
     public Guard() {
         this.compressor = new Compressor(6,PneumaticsModuleType.CTREPCM);
-        this.solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,1,2);
+        this.solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,4,3);
+        // in case above can ids are wrong
+        // this.solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,3,4);
     } 
 
     public void guardDown() {
-        solenoid.set(DoubleSolenoid.Value.kForward);
+        solenoid.set(DoubleSolenoid.Value.kReverse);
     }
     
     public void guardUp() { 
-        solenoid.set(DoubleSolenoid.Value.kReverse);
+        solenoid.set(DoubleSolenoid.Value.kForward);
     }
     
     @Override
